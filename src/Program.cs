59ce37408cfb34f8e787386c6eb28d9df4cb4a03
Rasmus Foged <rasmusf@upodi.com>
2024-02-18 17:18:24 +0100

    Adding dependencies

---
+++ src/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+if (Directory.Exists(Directory.GetCurrentDirectory() + "/runtime"))
+{
+    Console.WriteLine("wwwroot exists");
+}
+else
+{
+    Console.WriteLine("wwwroot does not exist");
+}
