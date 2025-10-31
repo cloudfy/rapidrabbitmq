812267725ceb55df32bbb663475f17431db6aac7
Rasmus Foged <rasmusf@plude.io>
2025-10-31 11:51:35 +0100

    WIP

--- src/StaticConstants.cs
+++ src/StaticConstants.cs
@@ -8,8 +8,8 @@ namespace RapidRabbitMQ;
 
 internal static class StaticConstants
 {
-    internal static string VERSION = "1.4.5";
-
-    internal static string ERLANG_BIN = "erts-15.1.2";
-    internal static string RMQ_VERSION = "rabbitmq_server-4.0.4";
+    internal static string VERSION = "1.5.0";
+    internal static string ERLANG_BIN = "erts-15.2.7.3";
+    internal static string RMQ_VERSION = "rabbitmq-server-windows-4.2.0";
+    internal static string ERL_URL = "";
 }
